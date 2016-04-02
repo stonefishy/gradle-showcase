e6de070f7920d7f9a2de4719efe57845beaf047e
Andrew <andrewsy@163.com>
2016-04-03 00:34:09 +0800

    added the dependency in gradle

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -1,10 +1,13 @@
 package hello;
 
+import org.joda.time.LocalTime;
 /**
  * Created by Andrewsy on 2016/4/2.
  */
 public class Greeter {
     public void sayHello() {
+        LocalTime currentTime = new LocalTime();
+        System.out.println("The current local time is:" + currentTime);
         System.out.println("Hello World");
     }
 }
