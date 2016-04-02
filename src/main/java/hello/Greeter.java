0ee8ee0b991ef5472ccace05ffe24afa48d8d3d2
Andrew <andrewsy@163.com>
2016-04-03 00:02:19 +0800

    added the java file.

---
+++ src/main/java/hello/Greeter.java
@@ -0,0 +1,10 @@
+package hello;
+
+/**
+ * Created by Andrewsy on 2016/4/2.
+ */
+public class Greeter {
+    public void sayHello() {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/main/java/hello/HelloWord.java
@@ -0,0 +1,11 @@
+package hello;
+
+/**
+ * Created by Andrewsy on 2016/4/2.
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        greeter.sayHello();
+    }
+}
